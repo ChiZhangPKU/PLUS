ff69ea0cd110881fbef4a76efa8ce936cb8bd11d
xiaoyulu95 <lu14@iu.edu>
2020-06-04 20:59:13 -0500

    PLUS

--- R/PLUS.R
+++ R/PLUS.R
@@ -1,15 +1,25 @@
-#train.X = stats::model.matrix(~.*.-1, as.data.frame(X)) # add 2-way interaction
-#train_data = cbind(train.X, X^2)
-#use: pred_AdaLabel=AdaLabel_PU(train_data=train_data,Label.obs=Label.obs,valid.id=valid.id,Sample_use_time=30,l.rate=1,qq=0.1)
+#usage: pred_PLUS=PLUS(train_data=train_data,Label.obs=Label.obs)
+#input 1. train_data N*M matrix which has N samples and M variables
+#input 2. Label.obs: Positive Unlabeled for each sample, 1 means true positive label, 0 means unlabeled labels
+#input 3. Sample_use_time: used in stop criteria, how many times each samples to be used in training process
+#input 4. l.rate: control how much information from last iteration will be used in next
+#input 5. qq: quantile of the probability for positive samples, used to determine the cutoff between positive and negtive
 
-AdaLabel_PU=function(train_data=train_data,Label.obs=Label.obs,valid.id=valid.id,Sample_use_time=30,l.rate=1,qq=0.1){
+#output 1. probability for each sample to be labeled as positive
+#output 2. cutoff to distinguish probability between positive and negtive samples
+#output 3. variable coeffecient non-zero means used in modeling
+
+library(glmnet)
+
+
+PLUS=function(train_data=train_data,Label.obs=Label.obs,Sample_use_time=30,l.rate=1,qq=0.1){
 
   N=dim(train_data)[1]
   Label=Label.obs
-
   train.X=train_data
-  
   pred.y0 = Label.obs
+  valid.id=which(pred.y0==1)
+  
   delta.p = array(Inf, N)
   PP = NULL
   invalid.id=seq(N)[-valid.id]
@@ -25,11 +35,10 @@ AdaLabel_PU=function(train_data=train_data,Label.obs=Label.obs,valid.id=valid.id
     #Used_time controls how many time each sample are selected
     prob_choosen[as.character(sample.id)]=prob_choosen[as.character(sample.id)]-(1/Sample_use_time)
     
-    # train
+    # train cv PLR model and do prediction
     fit.pi = cv.glmnet(train.X[c(valid.id, sample.id),], Label.obs[c(valid.id, sample.id)], family = "binomial")
     pred.y = predict(fit.pi, newx = train.X, s = "lambda.min", type = 'response')
-    #valid id in Label is true
-      #qq = ifelse(unique(Label[valid.id]) == 0, 0.9, 0.1)
+    #valid id in Label is true positive
     cutoff = quantile(pred.y[valid.id], qq)
     
     # rescale
@@ -40,25 +49,22 @@ AdaLabel_PU=function(train_data=train_data,Label.obs=Label.obs,valid.id=valid.id
     
     # record something
     delta.p[sample.id] = pred.y[sample.id] - pred.y0[sample.id]
-    # if there need learning rate
-    #l.rate controls chage rate from old to new
+    
+    # if there need learning rate: l.rate controls change rate from old to new
     pred.y0[sample.id] = pred.y[sample.id]*l.rate + (1-l.rate)*pred.y0[sample.id]
-    # shuffle
+    
+    # shuffle:only change label of sample.id
     Label.sample.id.old=Label.obs[sample.id]
     Label.obs[sample.id] = rbinom(length(sample.id), 1, pred.y0[sample.id])
-    Label.obs[valid.id] = 1
     Label.sample.id.new=Label.obs[sample.id]
     
-    #print(sum(Label.sample.id.old==Label.sample.id.new)/length(sample.id))
-    
-    #prob_choosen>=0
     prob_choosen[which(prob_choosen<=0)]=0
-    #if 90% of prob_choosen<=0.01 stop
+    #stop criteria 1: if 90% of prob_choosen<=0.01 stop
     if(sum(prob_choosen<=0.01)>(0.9*length(prob_choosen))){
       break
     }
+    #stop criteria 2: convergency
     change_propor=c(change_propor,sum(Label.sample.id.old==Label.sample.id.new)/length(sample.id))
-    #converge
     if(mean(change_propor[(i+1):(i+5)])>0.9)
     {
       break
@@ -68,20 +74,8 @@ AdaLabel_PU=function(train_data=train_data,Label.obs=Label.obs,valid.id=valid.id
   # overall model, use all data which is given accurate labels from previous steps
   fit.pi = cv.glmnet(train.X, Label.obs, family = "binomial")
   pred.y = predict(fit.pi, newx = train.X, s = "lambda.min", type = 'response')
-  
-  # if (length(unique(Label[valid.id])) > 1) {
-  #   valid.roc = roc(Label[valid.id], pred.y[valid.id], direction = "<", levels = c("0", "1"))
-  #   cutoff = valid.roc$thresholds[which.min((1-valid.roc$sensitivities)^2+(1-valid.roc$specificities)^2)]
-  # } else {
-  #   qq = ifelse(unique(Label[valid.id]) == 0, 0.9, 0.1)
-      cutoff = quantile(pred.y[valid.id], qq)
-  # }
-  
-  #pred.coef1=predict(fit.pi, newx = train.X,s = "lambda.min", type = "coefficients")
+  cutoff = quantile(pred.y[valid.id], qq)
   pred.coef1=coef(fit.pi, s = "lambda.min")
-  
-
-  #cutoff=cutoff
   
   return(list(pred.y=pred.y,cutoff=cutoff,pred.coef1=pred.coef1))
 
